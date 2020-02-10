9b266c6cfe20aee2075e7639482c7415f943cadc
Sameera-3 <sameeratasneem3@gmail.com>
2020-02-10 22:53:29 +0530

    initial commit

---
+++ Calculator2.java
@@ -0,0 +1,22 @@
+public class Calculator2{
+	public int add(double x,double y){
+		double z;
+		z=x+y;
+		return z;
+	}
+	public int sub(double x,double y){
+		double z;
+		z=x-y;
+		return z;
+	}
+	public int mul(double x,double y){
+		double z;
+		z=x*y;
+		return z;
+	}
+	public int div(double x,double y){
+		double z;
+		z=x/y;
+		return z;
+	}
+}
